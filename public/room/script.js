2218193ca76ca721bdc9ad03f3cd61306c804e9b
Noah <38923587+Noah-C-Z@users.noreply.github.com>
2020-11-30 15:03:36 -0500

    Talking indicator (#24)
    * Get speech border detection working for host
    
    * Update colour of border
    
    * Fix typo in menus.css
    
    * First attempt at volume detection
    
    * Create Analyzer object instead of AnalyzerNode
    
    * Remove log statements from whosTalking
    
    * Pass in actual analyzernode instead of user defined class
    
    * Join call muted and with no video
    
    * Remove username after disconnecting

--- public/room/script.js
+++ public/room/script.js
@@ -52,9 +52,16 @@ class Question {
     }
 }
 
+class Analyzer {
+    constructor(audioCtx) {
+        this.analyzer = audioCtx.createAnalyser();
+        this.analyzer.fftSize = 1024;
+        this.smoothing = 0;
+    }
+}
+
 const videoPositions = ['top-center', 'top-left', 'top-right', 'bottom-left', 'bottom-right'];
 let myVideoStream;
-let participantCount = 0;
 let handRaised = false;
 let participants = [];
 let questionQueue = [];
@@ -116,6 +123,51 @@ navigator.mediaDevices.getUserMedia({
         newPanner(-3,0,-1,3,0,1),   // hard left
         newPanner(3,0,1,-3,0,-1)    // hard right
     ];
+    const analyzers = [
+        new Analyzer(audioCtx),  // center
+        new Analyzer(audioCtx),  // soft left
+        new Analyzer(audioCtx),  // soft right
+        new Analyzer(audioCtx),  // hard left
+        new Analyzer(audioCtx)   // hard right
+    ];
+
+    const myAudio = audioCtx.createMediaStreamSource(stream);
+    const myAnalyzer = new Analyzer(audioCtx);
+    myAudio.connect(myAnalyzer.analyzer);
+
+    const processAudioVolume = (analyzer, pos) => {
+        const bufferLength = analyzer.analyzer.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+        analyzer.analyzer.getByteTimeDomainData(dataArray);
+        var meanSquared = 0;
+        dataArray.forEach(d => {
+            meanSquared += (d - 128) * (d - 128);
+        });
+        meanSquared = meanSquared / bufferLength;
+        // console.log(meanSquared);
+        // Magic number is the threshold for speech
+        if (meanSquared > 100){
+            document.getElementById(pos + '-video').style.border = '4px solid #46de40'
+        } else {
+            analyzer.smoothing += 1;
+            if (analyzer.smoothing == 50){
+                document.getElementById(pos + '-video').style.border = '0px';
+                analyzer.smoothing = 0;
+            }
+        }
+    }
+    const whosTalking = () => {
+        for (i = 0; i < participants.length; i++){
+            // console.log("Participant")
+            processAudioVolume(analyzers[i], videoPositions[i]);
+        }
+        // console.log("Host");
+        processAudioVolume(myAnalyzer, "bottom-center");
+        window.requestAnimationFrame(whosTalking);
+    }
+
+    whosTalking();
+
     const handAudioElement = document.createElement("audio");
     handAudioElement.src = "space_notif_final.wav";
     const handAudioSrc = audioCtx.createMediaElementSource(handAudioElement);
@@ -174,7 +226,10 @@ navigator.mediaDevices.getUserMedia({
         call.on('stream', userVideoStream => {
             //console.log(userVideoStream);
             //audioCtx.createMediaStreamSource(userVideoStream).connect(panners[0]).connect(hostDestination);
-            audioCtx.createMediaStreamSource(userVideoStream).connect(panners[participants.length - 1]).connect(audioCtx.destination);
+            audioCtx.createMediaStreamSource(userVideoStream)
+                .connect(analyzers[participants.length - 1].analyzer)
+                .connect(panners[participants.length - 1])
+                .connect(audioCtx.destination);
             console.log("On call");
             //hostDestination.stream.addTrack(videoTrack);
             //console.log(hostDestination.stream);
@@ -213,7 +268,10 @@ navigator.mediaDevices.getUserMedia({
         call.on('stream', userVideoStream => {
             //let videoTrack = userVideoStream.getVideoTracks()[0];
             // console.log(participants.length);
-            audioCtx.createMediaStreamSource(userVideoStream).connect(panners[participants.length - 1]).connect(audioCtx.destination);
+            audioCtx.createMediaStreamSource(userVideoStream)
+                .connect(analyzers[participants.length - 1].analyzer)
+                .connect(panners[participants.length - 1])
+                .connect(audioCtx.destination);
             console.log("User connected");
             //hostDestination.stream.addTrack(videoTrack);
             // addVideoStream(position, userVideoStream, newPart.hand);
@@ -256,6 +314,9 @@ navigator.mediaDevices.getUserMedia({
             }
         }
     })
+
+    muteUnmute();
+    playStop();
 })
 
 const removeQuestionFromQueue = (userId) => {
@@ -292,6 +353,7 @@ socket.on('user-disconnected', userId => {
         const discUser = participants[userIndex];
         discUser.video.style.display = "none";
         document.getElementById(videoPositions[userIndex] + '-image').style.display = "flex";
+        document.getElementById(videoPositions[userIndex] + '-name').innerHTML = "";
         participants.splice(userIndex, 1);
         // console.log("After splice");
         // console.log(participants);
