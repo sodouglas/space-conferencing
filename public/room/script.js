12111c7a101219bfaa8a3e966a9cca7cea60fa4b
Noah-C-Z <nczwack@gmail.com>
2020-12-01 00:39:48 -0500

    Color not formatted

--- public/room/script.js
+++ public/room/script.js
@@ -485,7 +485,7 @@ function handleError(error) {
 }
 
 shareButton.addEventListener('click', () => {
-    if (shareButton.querySelector(".fa-laptop").style.color === '#2be028'){
+    if (shareButton.querySelector(".fa-laptop").style.color === '2be028'){
         endScreenShare('');
     } else {
         navigator.mediaDevices.getDisplayMedia({
